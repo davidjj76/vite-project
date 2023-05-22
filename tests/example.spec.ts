fc7f4195bff6ade0c6f07d066af50ec849005afc
davidjj76 <davidjj76@gmail.com>
2023-05-22 11:46:17 +0200

    Playwright

---
+++ tests/example.spec.ts
@@ -0,0 +1,23 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Vite \+ React \+ TS/);
+});
+
+test('button', async ({ page }) => {
+  await page.goto('/');
+
+  // Click the button.
+  const button = page.getByRole('button');
+
+  // Expects the button to update the count.
+  expect(button).toHaveText('count is 0');
+
+  button.click();
+
+  // Expects the button to update the count.
+  await expect(button).toHaveText('count is 1');
+});
